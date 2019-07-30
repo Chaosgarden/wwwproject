cebba9e46eaa1016a2bf3750bcdceb9a88cc0786
Eric <EricTheDevil@hotmail.com>
2019-07-30 21:13:09 +0300

    cleaning up code

--- content.php
+++ content.php
@@ -9,43 +9,41 @@ if (mysqli_connect_error())
 }
 else
 {
-		//mysql query
-		$sql = "SELECT * FROM movies";
-		$result = $db->query($sql);
+	//mysql query
+	$sql = "SELECT * FROM movies";
+	$result = $db->query($sql);
 
-		if ($result->num_rows > 0) 
+	if ($result->num_rows > 0) 
+	{
+		// data of each row
+		while($row = $result->fetch_assoc()) 
 		{
-			// data of each row
-			while($row = $result->fetch_assoc()) 
-			{
-?>
-	<section class="container">
-		<div class="row">
-			
-			<!--flexbox is prob a better way, whitespace placeholder(prob not the best way-->
-			<div class="col-2">
-			</div>
-<?php
-				//the contents for the movie is put here
-				// example
-				echo $row["title"]. $row["category"]. $row["shortDescription"]. $row["link"]. "Directors: ". $row["link"]. "Stars: ". $row["link"];
-?>				
-		<div class="col-md-8">
-			<img> </img>
-		</div>
+			?>
+			<section class="container">
+				<div class="row">
 		
-		<!--whitespace placeholder(prob not the best way-->
-			<div class="col-2">
-			</div>
-		</div>
-	</section>
+					<!--flexbox is prob a better way, whitespace placeholder(prob not the best way-->
+					<div class="col-2">
+					</div>
+						
+					<?php echo $row["title"]. $row["category"]. $row["shortDescription"]. $row["link"]. "Directors: ". $row["link"]. "Stars: ". $row["link"]; ?>				
+					
+					<div class="col-md-8">
+						<img> </img>
+					</div>
+				
+					<!--whitespace placeholder(prob not the best way-->
+					<div class="col-2">
+					</div>
+				</div>
+			</section>
 <?php	
 
-			}
-		} else 
-		{
-			echo "0 results";
 		}
+	} else 
+	{
+		echo "0 results";
+	}
 			
 	$db->close();
 
