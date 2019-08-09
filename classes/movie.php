eedac12534cecb50e1f039ecece1154b7ede7af9
Eric <EricTheDevil@hotmail.com>
2019-08-09 22:50:45 +0300

    adding getters and setters for Movie

--- classes/movie.php
+++ classes/movie.php
@@ -1,26 +1,58 @@
 <?php
 class Movie
 {
-	public $title;
- 	public $image;	
-	public $fullDescription;
-	public $shortDescription;
-	public $category;
-	public $yearOfWork;
-	public $movieLength;
-	public $link;
+	private $title;
+ 	private $image;	
+	private $fullDescription;
+	private $shortDescription;
+	private $category;
+	private $yearOfWork;
+	private $movieLength;
+	private $link;
  
 	function __construct( 
 		$title, $image, $fullDescription, $shortDescription, $category, $yearOfWork, $movieLength, $link ) 
 	{
-		$this->image = $image;
 		$this->title = $title;
+		$this->image = $image;
 		$this->fullDescription = $fullDescription;
 		$this->shortDescription = $shortDescription;
 		$this->category = $category;
 		$this->yearOfWork = $yearOfWork;
 		$this->movieLength = $movieLength;
 		$this->link = $link;
+	}
+	public function getTitle()
+	{
+		return  $this->title;
+	}
+	function getImage()
+	{
+		return  $this->image;
+	}
+	function getFullDescript()
+	{
+		return  $this->fullDescription;
+	}
+	function getShortDescript()
+	{
+		return  $this->shortDescription;
+	}
+	function getCategory()
+	{
+		return  $this->category;
+	}
+	function getYearOfWork()
+	{
+		return  $this->yearOfWork;
+	}
+	function getMovieLength()
+	{
+		return  $this->movieLength;
+	}
+	function getLink()
+	{
+		return  $this->link;
 	}
 }
 //$first = new Movie("5", "JonWick", "0","89", "po", "o3", "Yes", "No" );
