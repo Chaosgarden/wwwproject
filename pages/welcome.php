1d775f7d9b609628149a06786553780eb1e2b775
Eric <EricTheDevil@hotmail.com>
2019-08-16 19:24:01 +0300

    changes

--- pages/welcome.php
+++ pages/welcome.php
@@ -8,7 +8,7 @@
    </head>
    <body>
 		<h1>Welcome <?php echo $login_session; ?> </h1> 
-		<h2><a href = "logout.php">Sign Out</a></h2>
+		<h2><a href = "../scripts/logout.php">Sign Out</a></h2>
 		
 		<section class="container">
 			<a href="../pages/addMovie.php" class="btn btn-default">Go to Google</a>
