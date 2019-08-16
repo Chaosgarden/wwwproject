8d6761d40a8523f192d52b3c7f3838d8cf404b2d
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2019-08-16 18:31:51 +0300

    Minor changes on CSS and Content Page Update 16.8.2019

--- header.php
+++ header.php
@@ -10,12 +10,39 @@
 <header class="container-fluid">
 	<div class="row">
 		<div class="col-2">
-			<a href="/wwwproject/index.php" class="btn btn-default" role="button">Movie Gallery </a>
+			<a href="/wwwproject/index.php" class="btn btn-primary" role="button">Movie Gallery </a>
 		</div>
 		
 		<div class="col-8 ">
-			<input type="input" placeholder="Movie Title"> 
-			<select> </select>
+			<div class="optionsTab">
+				<input type="input" placeholder="Movie Title"> 
+				<select name="category">
+					<option value="absurdist">Absurdist </option>
+					<option value="action">Action </option>
+					<option value="adventure">Adventure </option>
+					<option value="comedy">Comedy </option>
+					<option value="crime">Crime </option>
+					<option value="drama">Drama </option>
+					<option value="dantasy">Fantasy </option>
+					<option value="historical">Historical </option>
+					<option value="Historical fiction">Historical fiction </option>
+					<option value="Horror">Horror </option>
+					<option value="Magical realism">Magical realism </option>
+					<option value="Mystery">Mystery </option>
+					<option value="Paranoid fiction">Paranoid fiction </option>
+					<option value="Philosophical">Philosophical </option>
+					<option value="Political">Political </option>
+					<option value="Romance">Romance </option>
+					<option value="Saga">Saga </option>
+					<option value="Satire">Satire </option>
+					<option value="Science fiction">Science fiction </option>
+					<option value="Social">Social </option>
+					<option value="Speculative">Speculative </option>
+					<option value="Thriller">Thriller </option>
+					<option value="Urban">Urban </option>
+					<option value="Western">Western </option>
+				</select>
+			</div>
 		</div>
 		
 		<div class="col-2">
@@ -29,3 +56,4 @@
 		</div>
 	</div>
 </header>
+
