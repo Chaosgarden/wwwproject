fbc309cd1750cf89fb204707b5580e14954e16ec
Eric <EricTheDevil@hotmail.com>
2019-09-01 03:38:14 +0300

    added author class

---
+++ classes/artists.php
@@ -0,0 +1,58 @@
+<?php
+class Movie
+{
+	private $firstName;
+ 	private $lastName;	
+	private $nationality;
+	private $yearOfBirth;
+	private $yearofDeath;
+	private $biography;
+	private $picture;
+ 
+	function __construct( 
+		$firstName, $lastName, $nationality, $yearOfBirth, $category, $yearofDeath, $biography, $picture ) 
+	{
+		$this->firstName=$firstName;
+		$this->lastName=$lastName;
+		$this->nationality=$nationality;
+		$this->yearOfBirth=$yearOfBirth;
+		$this->yearofDeath=$yearofDeath;
+		$this->biography=$biography;
+		$this->picture=$picture;
+	}
+	function getFirstName()
+	{
+		return  $this->firstName;
+	}
+	function getLastName()
+	{
+		return  $this->lastName;
+	}
+	function getNationality()
+	{
+		return  $this->nationality;
+	}
+	function getYearOfBirth()
+	{
+		return  $this->yearOfBirth;
+	}
+	function getYearOfDeath()
+	{
+		return  $this->yearofDeath;
+	}
+	function getBiography()
+	{
+		return  $this->biography;
+	}
+	function getPicture()
+	{
+		return  $this->picture;
+	}
+	function getLink()
+	{
+		return  $this->links;
+	}
+}
+//$first=new Movie("5", "JonWick", "0","89", "po", "o3", "Yes", "No" );
+//echo $first->title;
+?>
