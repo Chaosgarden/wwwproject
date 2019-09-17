81ffa00837917afd717727e287a2a422e9411298
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2019-09-17 19:16:23 +0300

    Search changes 12.9.2019

--- pages/search.php
+++ pages/search.php
@@ -13,14 +13,19 @@ include_once '../classes/artist.php';
 		//$sql="SELECT * FROM artist where firstName='$searchText' OR lastName='$searchText ";
 		if($searchType == "movie")
 		{
-			$sql="SELECT * FROM movies where title='$searchText'";
+			$sql="SELECT * FROM movies where title LIKE '$searchText'";
 			$result=$conn->query($sql);		
 		}
 		if($searchType == "artist")
 		{
-			$sql="SELECT * FROM artist where firstName='$searchText'";
+			$sql="SELECT * FROM artist where (firstName LIKE '$searchText' OR lastname LIKE '$searchText')";
 			$result=$conn->query($sql);	
-		}	
+		}
+		if($searchType == "all")
+		{
+			$sql="SELECT * FROM artist a, movies m where (a.firstName LIKE '$searchText' OR a.lastname LIKE '$searchText' OR m.title LIKE '$searchText')";
+			$result=$conn->query($sql);	
+		}
 	}
 ?>
 <html>
@@ -45,7 +50,7 @@ if ($result=$conn->query($sql))
 				<div class="col-12">
 					<div class="row">
 						<div class="col-4">
-							<p> <?php echo $Movies->getImage(); ?> </p>
+							<img id="contentImg" src="<?php echo $Movies->getImage(); ?>">  </img>
 						</div>
 						<div class="col-8">
 							<table>
@@ -74,7 +79,6 @@ if ($result=$conn->query($sql))
 						</div>
 					</div>
 				</div>
-				<!--whitespace placeholder(prob not the best way-->
 			</div>
 		</section>
 	<?php		} ?>
@@ -132,7 +136,104 @@ if ($result=$conn->query($sql))
 			</div>
 		</section>
 				
-<?php		}	?>		
+<?php		}	?>	
+<?php
+		if($searchType == "all")
+		{
+			$Movies=new Movie($row["title"], $row["movieImage"],$row["fullDescription"],$row["shortDescription"], $row["category"], $row["yearOfWork"],$row["movieLength"], $row["link"]);
+		?>
+		<section class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="row">
+						<div class="col-4">
+							<img id="contentImg" src="<?php echo $Movies->getImage(); ?>">  </img>
+						</div>
+						<div class="col-8">
+							<table>
+								<tr>
+									<th>
+									<p> <?php echo $Movies->getTitle(); ?> </p>
+									</th>
+								</tr>
+								<tr>
+									<td>
+										<p> <?php echo $Movies->getMovieLength(); ?>min - <?php echo $Movies->getCategory(); ?> </p>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<p> <?php echo $Movies->getShortDescript(); ?> </p>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="<?php echo $Movies->getLink(); ?>" class="btn btn-info" role="button">Movie Trailer </a>
+									</td>
+								</tr>
+							
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	<?php		} ?>	
+	<?php
+			if($searchType == "all")
+			{	
+				$Artists=new Artist($row["firstName"], $row["lastName"],$row["nationality"],$row["yearOfBirth"], $row["yearOfDeath"], $row["biography"],$row["picture"]);
+			?>
+			<section class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="row">
+						<div class="col-4">
+							<img id="contentImg" src="<?php echo $Artists->getPicture(); ?>">  </img>
+						</div>
+						<div class="col-8">
+							<div class="table-responsive">
+								<table class="table borderless">
+									<tr>
+										<th>
+										<p> <?php echo $Artists->getFirstName(); ?> </p>
+										</th>
+									</tr>
+									<tr>
+										<td>
+											<p> <?php echo $Artists->getLastName(); ?></p>
+										</td>
+									</tr>
+									<tr>
+										<td>
+											<p> <?php echo $Artists->getNationality(); ?> </p>
+										</td>
+									</tr>
+									<tr>
+										<td>
+											<p> <?php echo $Artists->getYearOfBirth(); ?> </p>
+										</td>
+									</tr>
+									<tr>
+										<td>
+											<p> <?php echo $Artists->getYearOfDeath(); ?> </p>
+										</td>
+									</tr>
+									<tr>
+										<td>
+											<p> <?php echo $Artists->getBiography(); ?> </p>
+										</td>
+									</tr>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--whitespace placeholder(prob not the best way-->
+			</div>
+		</section>
+				
+<?php		}	?>	
 <?php include '../footer.php' ?>
 <?php	
 	}
