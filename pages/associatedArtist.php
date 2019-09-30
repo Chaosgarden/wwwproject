4693a9cd8db85c27a0fd81a6cd14e47fc185db7d
Eric <EricTheDevil@hotmail.com>
2019-10-01 00:08:40 +0300

    o

--- pages/associatedArtist.php
+++ pages/associatedArtist.php
@@ -11,7 +11,7 @@ include '../header.php';
 	//	$sql="SELECT * FROM orders where movieID='$movieID'";
 		$sql="INSERT INTO orders(movieID, artistID)
 			SELECT 'Joe The Student', id_teacher
-			FROM TAB_TEACHER
+			FROM 
 			WHERE movieID = '10'";
 		echo $movieID;
 		echo $artistID;
@@ -21,7 +21,27 @@ include '../header.php';
 				
 				echo $row["pee"];
 			}
+		/*
+CREATE TABLE employee (
+    employee_id INTEGER PRIMARY KEY,
+    employee_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE company (
+    company_id INTEGER PRIMARY KEY,
+    company_name VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE company_employee (
+    employee_id INTEGER NOT NULL,
+    company_id INTEGER NOT NULL,
+    work_hour_start TIME NOT NULL,
+    work_hour_end TIME NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES employee (employee_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (company_id) REFERENCES company (company_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY (employee_id, company_id, work_hour_start, work_hour_end));
 	
+*/	
 	}
 	if (isset($_POST['submit']))
 	{    
